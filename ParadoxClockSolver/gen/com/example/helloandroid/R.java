8bed130a44e6e5fba4c4e9909d7ceba45cfcaee0
Jussi Niskanen <jussini@gmail.com>
2012-04-17 15:36:15 +0300

    better ui

--- ParadoxClockSolver/gen/com/example/helloandroid/R.java
+++ ParadoxClockSolver/gen/com/example/helloandroid/R.java
@@ -8,25 +8,41 @@
 package com.example.helloandroid;
 
 public final class R {
+    public static final class array {
+        public static final int slot_values=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int editText1=0x7f050000;
-        public static final int slot1=0x7f050001;
-        public static final int slot10=0x7f05000a;
-        public static final int slot11=0x7f05000b;
-        public static final int slot12=0x7f05000c;
-        public static final int slot2=0x7f050002;
-        public static final int slot3=0x7f050003;
-        public static final int slot4=0x7f050004;
-        public static final int slot5=0x7f050005;
-        public static final int slot6=0x7f050006;
-        public static final int slot7=0x7f050007;
-        public static final int slot8=0x7f050008;
-        public static final int slot9=0x7f050009;
+        public static final int editText1=0x7f060000;
+        public static final int scrollView1=0x7f060001;
+        public static final int spinner1=0x7f060003;
+        public static final int spinner10=0x7f060015;
+        public static final int spinner11=0x7f060017;
+        public static final int spinner12=0x7f060019;
+        public static final int spinner2=0x7f060005;
+        public static final int spinner3=0x7f060007;
+        public static final int spinner4=0x7f060009;
+        public static final int spinner5=0x7f06000b;
+        public static final int spinner6=0x7f06000d;
+        public static final int spinner7=0x7f06000f;
+        public static final int spinner8=0x7f060011;
+        public static final int spinner9=0x7f060013;
+        public static final int textView1=0x7f060002;
+        public static final int textView10=0x7f060014;
+        public static final int textView11=0x7f060016;
+        public static final int textView12=0x7f060018;
+        public static final int textView2=0x7f060004;
+        public static final int textView3=0x7f060006;
+        public static final int textView4=0x7f060008;
+        public static final int textView5=0x7f06000a;
+        public static final int textView6=0x7f06000c;
+        public static final int textView7=0x7f06000e;
+        public static final int textView8=0x7f060010;
+        public static final int textView9=0x7f060012;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -34,5 +50,17 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int slot10_prompt=0x7f04000b;
+        public static final int slot11_prompt=0x7f04000c;
+        public static final int slot12_prompt=0x7f04000d;
+        public static final int slot1_prompt=0x7f040002;
+        public static final int slot2_prompt=0x7f040003;
+        public static final int slot3_prompt=0x7f040004;
+        public static final int slot4_prompt=0x7f040005;
+        public static final int slot5_prompt=0x7f040006;
+        public static final int slot6_prompt=0x7f040007;
+        public static final int slot7_prompt=0x7f040008;
+        public static final int slot8_prompt=0x7f040009;
+        public static final int slot9_prompt=0x7f04000a;
     }
 }
--- ParadoxClockSolver/src/net/ankkatalo/paradox/ClockSolverActivity.java
+++ ParadoxClockSolver/src/net/ankkatalo/paradox/ClockSolverActivity.java
@@ -7,7 +7,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.helloandroid.R;
@@ -34,125 +39,129 @@ public class ClockSolverActivity extends Activity {
         mSlots.add(new Slot(0));
         mSlots.add(new Slot(0));
         mSlots.add(new Slot(0));
-/*
-        StringBuilder builder = new StringBuilder();
-        for (int i= 0; i < slots.size(); ++i) {
-        	if (ClockSolver.solve(slots, i, 1)) {
-        		System.out.println("Found a solution");
-        		for (SolutionItem si: ClockSolver.solution) {
-        			builder.append(String.format("index: %d, value: %d\n", si.index,  si.value));
-        			//System.out.println(String.format("index: %d, value: %d", si.index,  si.value));        				
-        		}
+                
+        setContentView(R.layout.main);
+                
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        		this, R.array.slot_values, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        
+        Spinner sp = (Spinner) findViewById(R.id.spinner1);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(1));        
+        sp = (Spinner) findViewById(R.id.spinner2);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(2));
+        sp = (Spinner) findViewById(R.id.spinner3);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(3));
+        sp = (Spinner) findViewById(R.id.spinner4);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(4));
+        sp = (Spinner) findViewById(R.id.spinner5);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(5));        
+        sp = (Spinner) findViewById(R.id.spinner6);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(6));
+        sp = (Spinner) findViewById(R.id.spinner7);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(7));
+        sp = (Spinner) findViewById(R.id.spinner8);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(8));        
+        sp = (Spinner) findViewById(R.id.spinner9);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(9));        
+        sp = (Spinner) findViewById(R.id.spinner10);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(10));
+        sp = (Spinner) findViewById(R.id.spinner11);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(11));
+        sp = (Spinner) findViewById(R.id.spinner12);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new SlotSpinnerListener(12));        
+
+    }
+    
+    public void trySolving() {
+    	
+    	List<Slot> cloneList = new ArrayList<Slot>();
+    	cloneList.clear();
+    	int largest = 0;
+    	for (Slot slot: mSlots) {
+    		if (slot.value() > 0) {
+    			Slot newSlot = new Slot(slot);
+    			newSlot.setAvailable(true);
+    			cloneList.add(new Slot(slot));
+    			if (newSlot.value() > largest) {
+    				largest = newSlot.value();    	    				
+    			}
+    		}
+    	}
+
+    	TextView tv = (TextView) findViewById(R.id.editText1);
+    	
+    	if (cloneList.size() == 0) {
+    		tv.setText("Give some values to the clock face.");
+    		return;
+    	}
+    	    	
+    	if (cloneList.size() > 1 && largest > cloneList.size() / 2) {
+    		tv.setText(
+    				String.format("No solution, largest slot for clock size of %d is %d", 
+    				cloneList.size(), 
+    				cloneList.size()/2));
+    		return;    	    		
+    	}
+    	
+		ClockSolver.solution = new ArrayList<SolutionItem>();
+    	boolean foundSolution = false;
+        for (int i= 0; i < cloneList.size(); ++i) {
+        	if (ClockSolver.solve(cloneList, i, 1)) {
+        		foundSolution = true;
         		break;
         	}
         }
-        */
-                
-        setContentView(R.layout.main);
-        
-        EditText te = (EditText) findViewById(R.id.slot1);
-        te.addTextChangedListener(new SlotWatcher(1));
-        te = (EditText) findViewById(R.id.slot2);
-        te.addTextChangedListener(new SlotWatcher(2));
-        te = (EditText) findViewById(R.id.slot3);
-        te.addTextChangedListener(new SlotWatcher(3));
-        te = (EditText) findViewById(R.id.slot4);
-        te.addTextChangedListener(new SlotWatcher(4));
-        te = (EditText) findViewById(R.id.slot4);
-        te.addTextChangedListener(new SlotWatcher(5));
-        te = (EditText) findViewById(R.id.slot5);
-        te.addTextChangedListener(new SlotWatcher(6));
-        te = (EditText) findViewById(R.id.slot6);
-        te.addTextChangedListener(new SlotWatcher(7));
-        te = (EditText) findViewById(R.id.slot7);
-        te.addTextChangedListener(new SlotWatcher(8));
-        te = (EditText) findViewById(R.id.slot8);
-        te.addTextChangedListener(new SlotWatcher(9));
-        te = (EditText) findViewById(R.id.slot9);
-        te.addTextChangedListener(new SlotWatcher(10));
-        te = (EditText) findViewById(R.id.slot10);
-        te.addTextChangedListener(new SlotWatcher(11));
-        te = (EditText) findViewById(R.id.slot11);
-        te.addTextChangedListener(new SlotWatcher(12));
-        te = (EditText) findViewById(R.id.slot12);
-        
-        
-        //TextView tv = (TextView) findViewById(R.id.textview);
-        //tv.setText(builder.toString());        
+        if (foundSolution) {
+    		System.out.println("Found a solution");
+    		StringBuilder builder = new StringBuilder();
+    		for (SolutionItem si: ClockSolver.solution) {
+    			builder.append(String.format("index: %d, value: %d\n", si.index,  si.value));
+    			//System.out.println(String.format("index: %d, value: %d", si.index,  si.value));        				
+    		}    		
+    		tv.setText("");
+    		tv.setText(builder.toString());
+        } else {
+        	tv.setText("Given clock face is impossible to solve.");
+        }
+    	
     }
     
-
-    private class SlotWatcher implements TextWatcher {
+    
+    private class SlotSpinnerListener implements OnItemSelectedListener {
     	
-    	private int mSlot = -1;
-    	
-    	public SlotWatcher(int slot) {
-    		mSlot = slot;    		
+    	private int m_slot = -1;
+    	public SlotSpinnerListener (int slot) {
+    		this.m_slot = slot;    		
     	}
     	
-    	public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
-    			int arg3) {
-    		// TODO Auto-generated method stub    		
-    	}
+		public void onItemSelected(AdapterView<?> parent, View view, int pos,
+				long id) {
+			System.out.println("onItemSelected");
+			System.out.println(m_slot);
+    		String value = parent.getItemAtPosition(pos).toString();
+    		mSlots.get(m_slot-1).setValue(Integer.parseInt(value));
 
-    	public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
-    		// TODO Auto-generated method stub    		
-    	}
+			trySolving();			
+		}
 
-    	public void afterTextChanged(Editable s) {
-    	    	System.out.println(String.format("text of slot %d was changed to %s!", mSlot, s.toString()));
-    	    	
-    	    	if (s.toString().equalsIgnoreCase("")) {
-        	    	mSlots.get(mSlot-1).setValue(0);    	    		
-    	    	} else {
-        	    	mSlots.get(mSlot-1).setValue(Integer.parseInt(s.toString()));    	    		    	    		
-    	    	}
-    	    	
-    	    	List<Slot> cloneList = new ArrayList<Slot>();
-    	    	cloneList.clear();
-    	    	int largest = 0;
-    	    	for (Slot slot: mSlots) {
-    	    		if (slot.value() > 0) {
-    	    			Slot newSlot = new Slot(slot);
-    	    			newSlot.setAvailable(true);
-    	    			cloneList.add(new Slot(slot));
-    	    			if (newSlot.value() > largest) {
-    	    				largest = newSlot.value();    	    				
-    	    			}
-    	    		}
-    	    	}
-    	    	if (largest > cloneList.size() / 2.0) {
-    	    		System.out.println("No solution");
-    	    		return;    	    		
-    	    	}
-    	    	    	      	      	    	
-    	    	System.out.println(cloneList.size());
-    	    	
-        		ClockSolver.solution = new ArrayList<SolutionItem>();
-    	    	boolean foundSolution = false;
-    	        for (int i= 0; i < cloneList.size(); ++i) {
-    	        	if (ClockSolver.solve(cloneList, i, 1)) {
-    	        		foundSolution = true;
-    	        		break;
-    	        	}
-    	        }
-    	        if (foundSolution) {
-	        		System.out.println("Found a solution");
-	        		StringBuilder builder = new StringBuilder();
-	        		for (SolutionItem si: ClockSolver.solution) {
-	        			builder.append(String.format("index: %d, value: %d\n", si.index,  si.value));
-	        			//System.out.println(String.format("index: %d, value: %d", si.index,  si.value));        				
-	        		}
-	        		TextView tv = (TextView) findViewById(R.id.editText1);
-	        		tv.setText("");
-	        		tv.setText(builder.toString());
-    	        } else {
-    	        	System.out.println("No solution");    	        	
-    	        }
-
-    	}
-    	
-    	
+		public void onNothingSelected(AdapterView<?> arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+        	
     }
     
 }
